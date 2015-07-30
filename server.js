3b5699d54360c1078f47f68f58300455c902b38f
mpapousGit <m_papous@hotmail.com>
2015-07-30 17:52:40 +0100

    Progress on routing and serving the data

--- server.js
+++ server.js
@@ -1,18 +1,51 @@
+var fs = require('fs');
 var path = require('path');
 var express = require('express');
-var papaparse = require('papaparse');
+var Papa = require('papaparse');
 var app = express();
 
-// Require paparase
+var port = process.env.PORT || 3000;
+
+var contactRouter = express.Router();
 
 /**
  * GET /contacts
  */
+contactRouter.route('/contacts')
+  .get(function(req, res){
+    var data = fs.readFileSync('my-list.csv');
 
-// 1. Declare a GET route
-// 2. Read the CSV file
-// 3. Parse the CSV and convert to JSON
-// 4. Return the JSON response of that parsed CSV
+    var obj = Papa.parse(data.toString(), {
+     header: true,
+     delimiter: ",",
+     complete: function(results) {
+        res.send(results.data);
+     }
+    });
+  });
+
+contactRouter.route('/contacts/:firstname')
+  .get(function(req, res) {
+
+    var users = fs.readFileSync('my-list.csv');
+
+    var user = Papa.parse(users.toString(), {
+     header: true,
+     delimiter: ",",
+     complete: function(results) {
+      var findUserByname = function (firstname, callback) {
+        if (!results.data[firstname])
+          return callback(new Error(
+            'No user matching '
+             + firstname
+            )
+          );
+      };
+
+      res.send(results.data);
+     }
+    });
+});
 
 /**
  * GET /contacts/:firstname
@@ -25,8 +58,13 @@ var app = express();
 // 5. Return JSON
 // 6. If :firstname was not found in the CSV, return 404
 
-app.use(express.static(path.resolve(__dirname)));
 
-app.listen(3000, function() {
+app.use('/', contactRouter);
+
+app.get('/', function(req, res){
+  res.send('Welcome to my API');
+});
+
+app.listen(port, function() {
   console.log('listening');
 });
