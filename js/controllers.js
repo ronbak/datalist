e886b6182911917d26cced574e515a53a109fdd9
mpapousGit <m_papous@hotmail.com>
2015-09-12 22:49:51 +0100

    npm update

--- js/controllers.js
+++ js/controllers.js
@@ -2,16 +2,15 @@
 
   var app = angular.module('test');
 
-  app.controller('contactsCtrl', function($scope, $routeParams, ContactResource) {
-
-    $scope.contacts = ContactResource.all().query();
-
+  app.controller('contactsCtrl',
+    function($scope, $routeParams, ContactResource) {
+      $scope.contacts = ContactResource.all().query();
   });
 
-  app.controller('singleCtrl', function($scope, $routeParams, ContactResource) {
-
-    $scope.singlecontact = ContactResource.single().query({firstname: $routeParams.firstname});
-
+  app.controller('singleCtrl',
+    function($scope, $routeParams, ContactResource) {
+      $scope.singlecontact = ContactResource.single()
+            .query({firstname: $routeParams.firstname});
   });
 
 }());
